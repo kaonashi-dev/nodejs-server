c7edfca5f32b34285c180a3a78358183de462aa1
Samuel <samueldlh.dev@gmail.com>
2022-10-14 01:22:09 -0500

    static files

--- src/app/server.js
+++ src/app/server.js
@@ -1,3 +1,6 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import express from 'express';
 
 export default class Server {
@@ -6,7 +9,19 @@ export default class Server {
       this.port = process.env.PORT;
       this.app = express();
 
+      // Middlewares
+      this.middlewares();
+
+      // Routes app
       this.routes();
+   }
+
+   middlewares() {
+
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = path.dirname(__filename);
+
+      this.app.use(express.static(path.join(__dirname, 'public')))
    }
 
    routes() {
